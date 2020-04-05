2c7abfa7492b4a9d86662cc6a2618a7fc3589719
vanessa <jvtrivinog@unal.edu.com>
2020-04-05 02:04:43 -0500

    :construction: Componente navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <ul class="nav justify-content-end" id="nav1">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Amazonas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Andina</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Caribe</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pacifico</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Orinoquia</a>
-        </li>
-      </ul>
-
       <header className="App-header">
-
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
