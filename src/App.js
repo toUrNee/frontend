4c45464cc1fde3ff3f23a5c7a561bdb0c4ef4517
banterm <mbsuarezg@unal.edu.co>
2020-04-09 19:21:25 -0500

    Pantalla del login (responsive)

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register'
+import  LoginBox from './components/LoginBox'
 
 function App() {
   return (
     <div className="App">
-      <ul class="nav justify-content-end" id="nav1">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Amazonas</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Andina</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Caribe</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pacifico</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Orinoquia</a>
-        </li>
-      </ul>
-
-      <header className="App-header">
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div class = "row no-gutters">
+        <div class = "col-md-5 no-gutters">
+          <div class = "left d-flex justify-content-center">
+            <Register />
+          </div>
+        </div>
+        <div class = "col-md-7 no-gutters">
+          <div class = "right">
+            <LoginBox />            
+          </div>
+        </div>
+      </div>   
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const Register = () =>
+    <div> 
+        <h1 className = "leftTitle"> ¡Bienvenido a Cattleya tours! </h1>
+        <h2 className = "leftSub"> Inicia sesión para continuar</h2>
+        <button className = "leftButton"> Crear cuenta</button>
+    </div>
+
+export default Register
