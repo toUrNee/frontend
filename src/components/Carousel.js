a6198c4c0a0228d852a5c6d26f133de5de38ddc4
Farid Cortes <difcortesgu@unal.edu.co>
2020-04-12 01:43:53 -0500

    :boom: Modifique el carrusel para traer la informacion desde la base de datos

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,58 +10,83 @@ import {
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 
 
-function slide(region, img, clase) {
+function slide(id, region, img, clase) {
     var slide =
-        <div className={clase}>
+        <div key={id} className={clase}>
             <img src={img} alt={img} ></img>
             <div className="carousel-caption">
                 <h1>{region}</h1>
-                <Link to ={`/publicaciones/${region}`}>
+                <Link  to ={{
+                    pathname:`/publicaciones/${region}`,
+                    state:{
+                        region_id:{id}
+                    }
+                }}>
                 <button type="button" className="btn ">Ver planes</button>                
                 </Link>
                 
             </div>
         </div>
-
-        return slide;
+    return slide;
 }
 
 class Carousel extends Component {
+
+    state =  {
+        loading: true,
+        regiones: []
+    };
+
+    async componentDidMount(){
+        const base_url='https://localhost:5001/api'
+        const response = await fetch (base_url+'/Regiones')
+        const data = await response.json()
+        this.setState({loading: false, regiones: data})
+    }
+
     render() {
-        return (
-            <div className="row">
-                <div id="demo" className="carousel slide" data-ride="carousel">
-                    <ul className="carousel-indicators">
-                        <li data-target="#demo" data-slide-to="0" className="active"></li>
-                        <li data-target="#demo" data-slide-to="1"></li>
-                        <li data-target="#demo" data-slide-to="2"></li>
-                        <li data-target="#demo" data-slide-to="3"></li>
-                        <li data-target="#demo" data-slide-to="4"></li>
+        if (this.state.loading){
+            return(<div>Cargando ...</div>)
+        }else{
+            const panels = this.state.regiones.map((region) => {
+                let img = null
+                let clase = 'carousel-item'
+                switch (region.nombre) {
+                    case "Amazonas": img = amazonas; clase = clase.concat(' active'); break;
+                    case "Orinoquia": img = orinoquia; break;
+                    case "Andina": img = andina; break;
+                    case "Caribe": img = caribe; break;
+                    case "Pacifico": img = pacifico; break;
+                }
+                return slide(region.id, region.nombre, img, clase)
+            })
+            return (
+                <div className="row">
+                    <div id="demo" className="carousel slide" data-ride="carousel">
+                        <ul className="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" className="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                            <li data-target="#demo" data-slide-to="3"></li>
+                            <li data-target="#demo" data-slide-to="4"></li>
 
-                    </ul>
-                    <div className="carousel-inner" id="regiones-slide">
-
-
-                        {slide("Amazonas", amazonas, "carousel-item active")}
-                        {slide("Andina", andina, "carousel-item ")}
-                        {slide("Caribe", caribe, "carousel-item ")}
-                        {slide("Orinoquia", orinoquia, "carousel-item ")}
-                        {slide("Pacifico", pacifico, "carousel-item ")}
-
-
+                        </ul>
+                        <div className="carousel-inner" id="regiones-slide">
+                            {panels}
+                        </div>
+                        <a className="carousel-control-prev" href="#demo" data-slide="prev">
+                            <span className="carousel-control-prev-icon"></span>
+                        </a>
+                        <a className="carousel-control-next" href="#demo" data-slide="next">
+                            <span className="carousel-control-next-icon"></span>
+                        </a>
                     </div>
-                    <a className="carousel-control-prev" href="#demo" data-slide="prev">
-                        <span className="carousel-control-prev-icon"></span>
-                    </a>
-                    <a className="carousel-control-next" href="#demo" data-slide="next">
-                        <span className="carousel-control-next-icon"></span>
-                    </a>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
--- src/components/Region.js
+++ src/components/Region.js
@@ -9,9 +9,11 @@ import pacifico from '../images/pacifico.jpg'
 class Region extends Component {
 
     async componentDidMount(){
+        let region_id = this.props.location.state.region_id.id
         const base_url='https://localhost:5001/api'
-        const response = await fetch (base_url+'/Publicaciones/Region/3')
+        const response = await fetch (base_url+'/Publicaciones/region/'+region_id)
         const data = await response.json()
+        //Aqui esta la lista de las publicaciones por region
         console.log(data)
     }
 
