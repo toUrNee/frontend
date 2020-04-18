e6ebce171b8db91e888f2520f30e2c786e3b64c2
vanessa <jvtrivinog@unal.edu.com>
2020-04-17 22:02:30 -0500

    :construction: Mejorando los formularios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import './styles/Form.css'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import img from '../images/imagen.png';
 
 class Formulario extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="container-fluid form-container ">
+                <div className="row align-items-center">
+                    <div className="col col-color">
+                        <header>
+                            <h1 className="titulo-form">TITULO FORMULARIO</h1>
+                            <img className="img-fluid mx-auto d-block img-form" src={img} />
+                        </header>
+                    </div>
+                    <div className="col col-form ">
+                        <form>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleFormControlInput1">Email address</label>
+                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleFormControlSelect1">Example select</label>
+                                <select className="form-control" id="exampleFormControlSelect1">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                            <div className="form-check">
+                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
